1554f501d05c0673baee9feb38263b293c133efb
rajinkhatri <61622498+rajinkhatri@users.noreply.github.com>
2022-11-18 08:53:44 +0545

    added the movement and drawing method

---
+++ car.js
@@ -0,0 +1,84 @@
+class Car {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.speed = 0;
+        this.acceleration = 0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
+        this.angle = 0;
+
+        this.controls = new Controls;
+    }
+
+    update() {
+        this.#move();
+    }
+
+    // every controls of the car //
+    #move() {
+        if (this.controls.forward) {
+            this.speed += this.acceleration;
+        }
+
+        if (this.controls.reverse) {
+            this.speed -= this.acceleration;
+        }
+
+        if (this.speed > this.maxSpeed) {
+            this.speed = this.maxSpeed;
+        }
+
+        if (this.speed < -this.maxSpeed / 2) {
+            this.speed = -this.maxSpeed / 2;
+        }
+
+        if (this.speed > 0) {
+            this.speed -= this.friction;
+        }
+
+        if (this.speed < 0) {
+            this.speed += this.friction;
+        }
+
+        if (Math.abs(this.speed) < this.friction) {
+            this.speed = 0;
+        }
+        
+        // to maintain the direction of the car when its going in reverse, the flip value chnages the direction when speed < 0 //
+        if (this.speed != 0) {
+            const flip = this.speed > 0 ? 1 : -1;
+            if (this.controls.right) {
+                this.angle -= 0.03 * flip;
+            }
+
+            if (this.controls.left) {
+                this.angle += 0.03 * flip;
+            }
+        }
+
+        // chnages the value of the x and y once all other calculations has been done; based on unit circle method for the movement // 
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
+    }
+
+    draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y); // like printing the value there //
+        ctx.rotate(-this.angle); // rotating the car right or left //
+
+        ctx.beginPath();
+        ctx.rect(
+            -this.width / 2,
+            -this.height / 2,
+            this.width,
+            this.height
+        );
+        ctx.fill();
+
+        ctx.restore();
+    }
+}
