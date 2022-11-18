b7b19e26f0942abcb7436140f714ff25dc5b0485
rajinkhatri <61622498+rajinkhatri@users.noreply.github.com>
2022-11-18 18:35:48 +0545

    updated new way of drawing the car

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-const car = new Car(100,100,30,50);
+const road = new Road(canvas.width/2, canvas.width*0.9);
+const car = new Car(road.getLaneCenter(1),100,30,50);
 
 animate();
 
 function animate(){
-    car.update();
+    car.update(road.borders);  
     canvas.height = window.innerHeight;  // for the full height of the windows//
+    ctx.save();
+    ctx.translate(0, -car.y+canvas.height*0.7);
+    road.draw(ctx);
     car.draw(ctx); // takes value from car and uses context from ctx to draw it //
+
+    ctx.restore();
     requestAnimationFrame(animate);
 }
