b1839c695983a8da63db9564d3b672186bd42066
rajinkhatri <61622498+rajinkhatri@users.noreply.github.com>
2022-11-18 08:54:29 +0545

    added the control mechanisms for movement

---
+++ controls.js
@@ -0,0 +1,49 @@
+class Controls{
+    constructor(){
+        this.forward = false;
+        this.left = false;
+        this.right = false;
+        this.reverse = false;
+        
+        this.#addKeyboardListeners(); //the # makes it  private
+    }
+
+    #addKeyboardListeners(){
+
+        // To change the direction when key gets pressed 
+        document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left = true;
+                    break;
+                case "ArrowRight":
+                    this.right = true;
+                    break;
+                case "ArrowUp":
+                    this.forward = true;
+                    break;
+                case "ArrowDown":
+                    this.reverse = true;
+                    break;
+            }
+        }
+
+        // To change the direction when key gets unpressed 
+        document.onkeyup=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left = false;
+                    break;
+                case "ArrowRight":
+                    this.right = false;
+                    break;
+                case "ArrowUp":
+                    this.forward = false;
+                    break;
+                case "ArrowDown":
+                    this.reverse = false;
+                    break;
+            }
+        }
+    }
+}
